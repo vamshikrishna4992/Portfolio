e1b6df789c1d892a73da3dc77c7948cae55bbfb0
vamshikrishna4992 <vamshikrishna4992@gmail.com>
2024-11-13 23:39:36 +0530

    profile added

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Home.css";
 
+
 function Home() {
   const dynamicContent = {
     name: "Vamshi Krishna",
@@ -11,6 +12,7 @@ function Home() {
     <div className="home">
       <h1>Welcome to {dynamicContent.name}'s Portfolio</h1>
       <p>{dynamicContent.description}</p>
+      <img src='https://imgs.search.brave.com/xFkz2rHVRFxHB3pOHKPh-9VUyP9DKszbVpTUzIP9HvM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJzLmNvbS9p/bWFnZXMvZmVhdHVy/ZWQvY29vbC1wcm9m/aWxlLXBpY3R1cmUt/ODdoNDZnY29iamw1/ZTR4dS5qcGc' alt="profile" />
     </div>
   );
 }
